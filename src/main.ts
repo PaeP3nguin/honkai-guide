b26a3aafea26bd3a0ee5e41c50fbae3aa4989e35
WX <paep3nguin@gmail.com>
2020-01-14 19:54:53 -0800

    Add google analytics

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false;
 
 Vue.use(require("moment"));
+
+Vue.use(VueGtag, {
+  config: { id: "UA-57689110-5" }
+});
 
 new Vue({
   vuetify,
