49a17364d36da0c841dc99c92c765a5212f8eccc
WX <paep3nguin@gmail.com>
2020-02-02 01:33:33 -0800

    Update text for multipliers

--- src/data/valk_multipliers.ts
+++ src/data/valk_multipliers.ts
@@ -7,38 +7,38 @@ const VALK_MULTIPLIERS = {
         "Herrscher of Reason (HoR)": [
             new Multiplier({
                 active: true,
-                name: "HoR: Lead Skill",
+                name: "[HoR] Lead Skill",
                 type: Type.EleDealt,
                 value: 24,
             }),
             new Multiplier({
                 active: true,
-                name: "HoR: Lead Skill",
+                name: "[HoR] Lead Skill",
                 type: Type.TdmDealt,
                 value: 21,
             }),
             new Multiplier({
                 active: true,
-                name: "HoR Passive: Against analyzed enemies",
+                name: "[HoR] Passive: Against analyzed enemies",
                 type: Type.TdmDealt,
                 value: 40,
             }),
             new Multiplier({
                 active: true,
-                name: "HoR Chilling Build: Charge attacks",
+                name: "[HoR] Chilling Build: Charge attacks",
                 type: Type.EleDealt,
                 value: 54,
             }),
             new Multiplier({
                 active: true,
-                name: "HoR Speed Decypher: ult and charged attacks",
+                name: "[HoR] Speed Decypher: ult and charged attacks",
                 type: Type.TdmDealt,
                 value: 30,
             }),
             new Multiplier({
                 active: false,
                 name:
-                    "HoR Stream Transfer: Non-ult charge attacks (15%/stack, max 3 stacks)",
+                    "[HoR] Stream Transfer: Non-ult charge attacks (15%/stack, max 3 stacks)",
                 type: Type.EleDealt,
                 value: 45,
             }),
@@ -51,38 +51,38 @@ const VALK_MULTIPLIERS = {
         "Hawk of the Fog (HF)": [
             new Multiplier({
                 active: false,
-                name: "HF Unity Mantra: bonus when breaking shield, 12 seconds",
+                name: "[HF] Unity Mantra: bonus when breaking shield, 12 seconds",
                 type: Type.TdmDealt,
                 value: 30,
             }),
             new Multiplier({
                 active: true,
-                name: "HF Vajra Mantra: bonus lightning",
+                name: "[HF] Vajra Mantra: bonus lightning",
                 type: Type.EleDealt,
                 value: 30,
             }),
             new Multiplier({
                 active: true,
-                name: "HF Shattering Mantra: against shielded enemies",
+                name: "[HF] Shattering Mantra: against shielded enemies",
                 type: Type.TdmDealt,
                 value: 30,
             }),
             new Multiplier({
                 active: false,
                 name:
-                    "HF Flux Wym: bonus for Six-sided Shock when doing basic attacks during Flux Wyrm, 14%/stack, max 5 stacks",
+                    "[HF] Flux Wyrm: bonus for Six-sided Shock when doing basic attacks during Flux Wyrm, 14%/stack, max 5 stacks",
                 type: Type.EleDealt,
                 value: 70,
             }),
             new Multiplier({
                 active: true,
-                name: "HF Teravolt Maul: ult",
+                name: "[HF] Teravolt Maul: bonus on ultimate",
                 type: Type.TdmDealt,
                 value: 50,
             }),
             new Multiplier({
                 active: true,
-                name: "HF Conductive Impact: within 18s of entry or ult",
+                name: "[HF] Conductive Impact: within 18s of entry or ult",
                 type: Type.EleDealt,
                 value: 45,
             }),
