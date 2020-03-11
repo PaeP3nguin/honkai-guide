9d8bf5ddba97c9c9d1bb44ef5101a5d6e4ac8dfd
WX <paep3nguin@gmail.com>
2020-03-10 21:18:16 -0700

    Break out multiplier dialog component and use it for stigs

---
+++ src/components/MultiplierDialog.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-dialog scrollable v-model="dialogVisible" max-width="500px">
+    <template v-slot:activator="{ on }">
+      <v-btn color="primary" v-on="on">
+        <slot name="button"></slot>
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-card-title class="pb-0 flex-column align-stretch">
+        <span class="headline">{{ title }}</span>
+
+        <v-text-field label="Search" v-model="dialogFilter" autofocus></v-text-field>
+      </v-card-title>
+
+      <v-tabs v-model="activeTab">
+        <template v-for="(groups, type) in filteredDialogItems">
+          <v-tab v-bind:key="type">{{ type }}</v-tab>
+        </template>
+      </v-tabs>
+
+      <v-card-text class="pa-0">
+        <v-tabs-items v-model="activeTab">
+          <template v-for="(groups, type) in filteredDialogItems">
+            <v-tab-item class="px-4" v-bind:key="type">
+              <div v-for="(pieces, name, i) in groups" v-bind:key="name">
+                <v-divider v-if="i != 0" class="mt-4"></v-divider>
+
+                <slot name="group" v-bind:group="{pieces: pieces, name: name}"></slot>
+              </div>
+            </v-tab-item>
+          </template>
+        </v-tabs-items>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn text color="primary" @click="dialogVisible = false">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "MultiplierDialog",
+  props: {
+    title: String,
+    dps: Object,
+    support: Object
+  },
+  data: function() {
+    return { dialogVisible: false, dialogFilter: "", activeTab: null };
+  },
+  computed: {
+    filteredDialogItems: function() {
+      function filterByKeyName(map, filterString: string) {
+        return Object.keys(map)
+          .filter(key => key.toLowerCase().includes(filterString.toLowerCase()))
+          .reduce((obj, key) => {
+            obj[key] = map[key];
+            return obj;
+          }, {});
+      }
+
+      return {
+        DPS: filterByKeyName(this.dps, this.dialogFilter),
+        Support: filterByKeyName(this.support, this.dialogFilter)
+      };
+    }
+  }
+});
+</script>
