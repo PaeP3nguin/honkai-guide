8333b566b1ce8bce47dce18a06f90295e3b9907f
WX <paep3nguin@gmail.com>
2020-09-07 15:30:14 -0700

    Add CN 10/5 MA lineup

--- src/data/boss_lineups.ts
+++ src/data/boss_lineups.ts
@@ -160,6 +160,10 @@ const BOSS_LINEUPS = Object.freeze([
     bosses: [Bosses.HoV, Bosses.BKE, Bosses.AKA]
   }),
   new BossLineup({
+    date: new Date("2020-10-05T00:00:00"),
+    bosses: [Bosses.Hephaestus, Bosses.DXY, Bosses.AKA]
+  }),
+  new BossLineup({
     date: new Date("2020-10-29T00:00:00"),
     event: "Patch 4.3 (predicted)"
   })
