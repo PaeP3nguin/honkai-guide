2ff8f0ca509aa1f683102da26dd1459eca02e36f
Hau NT <hau.nguyenthe@gmx.de>
2022-12-02 14:13:36 +0100

    Add SSS Base score, remove SS Up
    Refactor score naming convention (small)

--- src/util/score_util.ts
+++ src/util/score_util.ts
@@ -1,16 +1,20 @@
-const pointsPerSecond = 53 + 1 / 3;
-const pointsPerSecondSSS = 16;
+const pointsPerSecondSS = 53 + 1 / 3;
+const pointsPerSecondBase = 13 + 1/3;
+//const pointsPerSecondUp = 16;
 
 function generateScores(seconds: number) {
   const scoresByTime = [];
   for (let i = 0; i < seconds; i += 1) {
-    const rawScore = 32000 - pointsPerSecond * (i + 1);
-    const sssScore = 48000 - pointsPerSecondSSS * (i + 1);
-    const score = Math.floor(rawScore);
-    const up = Math.round(rawScore * 1.2);
+    const ssScoreRaw = 32000 - pointsPerSecondSS * (i + 1);
+    const ssScore = Math.floor(ssScoreRaw);
+
+    const baseRaw = 40000 - pointsPerSecondBase * (i + 1);
+    const base = Math.floor(baseRaw);
+    const up = Math.round(baseRaw * 1.2);
+
     scoresByTime.push({
-      score: score,
-      sssScore: sssScore,
+      ssScore: ssScore,
+      base: base,
       up: up,
       elapsedSec: i,
     });
